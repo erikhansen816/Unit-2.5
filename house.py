fd697bc3a6187ae682cf16ac84f82d8a75cb4f8d
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-09-26 14:43:44 -0400

    Updated from Brython Server: 9/26/2017 2:43:42 PM

--- house.py
+++ house.py
@@ -8,9 +8,10 @@ blue = Color(0x0000FF,1)
 black = Color(0x000000,1)
 blackoutline = LineStyle(1,black)
 bluesquare = RectangleAsset(200,200,blackoutline,blue)
-bluetriangle = PolygonAsset([(25,150),(175,250),(325,150)],blackoutline,blue)
+bluetriangle = PolygonAsset([(25,150),(175,50),(325,150)],blackoutline,blue)
+blackdoor = RectangleAsset(50,75,blackoutline,black)
 
-
-Sprite(bluesquare)
+Sprite(bluesquare, (75,150))
 Sprite(bluetriangle)
+Sprite(blackdoor, (150,275))
 App().run()
