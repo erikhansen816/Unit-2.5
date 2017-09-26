4cad83c23748ee2f0ec7df49600f17c98e7faf19
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-09-26 14:35:44 -0400

    Updated from Brython Server: 9/26/2017 2:35:42 PM

--- house.py
+++ house.py
@@ -1,1 +1,16 @@
+#Erik Hansen
+#9/26/2017
+#house.py - making a house
 
+from ggame import *
+
+blue = Color(0x0000FF,1)
+black = Color(0x000000,1)
+blackoutline = LineStyle(1,black)
+bluesquare = RectangleAsset(200,200,blackoutline,blue)
+bluetriangle = PolygonAsset([(25,150),(175,250),(325,150)],blackoutline,blue)
+
+
+Sprite(bluesquare)
+Sprite(bluetriangle)
+App().run()
